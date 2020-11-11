01d2441a642a0364e16ec1ee0f0397be7fe62fec
Farid <farid.hammouten@gmail.com>
2020-11-12 07:28:42 +0900

    Basic Home Page

---
+++ src/views/HomePage/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+const Home = () =>(
+    <Container>
+        <h1>Home Page</h1>
+    </Container>
+);
+
+export default Home;
