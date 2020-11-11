3074f5d10de113786acd216f6684e6b20e07dc44
Farid <farid.hammouten@gmail.com>
2020-11-12 07:29:30 +0900

    Basic Faq Page

---
+++ src/views/FaqPage/Faq.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+const Faq = () =>(
+    <Container>
+        <h1>Faq Page</h1>
+    </Container>
+);
+
+export default Faq;
