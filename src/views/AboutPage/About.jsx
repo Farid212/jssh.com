c34cb50ebf05b2b95934a945d0767113528987ec
Farid <farid.hammouten@gmail.com>
2020-11-12 07:29:02 +0900

    Basic About Page

---
+++ src/views/AboutPage/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+const About = () =>(
+    <Container>
+        <h1>About Page</h1>
+    </Container>
+);
+
+export default About;
