a75d879fa9fea3082d87d857c485060de81efa29
Farid <farid.hammouten@gmail.com>
2020-11-14 03:49:55 +0900

    jumbo updated

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { MDBJumbotron, MDBBtn, MDBContainer, MDBRow, MDBCol, MDBCardTitle, MDBIcon } from "mdbreact";
+import { MDBJumbotron, MDBBtn, MDBCol, MDBIcon } from "mdbreact";
 
 const Jumbotron = () => {
   return (
     <MDBJumbotron style={{ padding: 0 }}>
       <MDBCol className="text-white text-center py-5 px-4" style={{ backgroundColor: "#ffe51f" }}>
         <MDBCol className="py-5">
-          <MDBCardTitle style={{color: "black"}} className="h1-responsive pt-3 m-5 font-bold">Become a JavaScript Super Hero!</MDBCardTitle>
+          <h1 style={{color: "black"}} className="pt-3 m-5 font-bold">Become a JavaScript Super Hero!</h1>
           <p className="mx-5 mb-5"  style={{color: "black"}} >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
             optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
           </p>
-          <MDBBtn outline color="white" className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn>
+          <MDBBtn outline style={{border: "1px solid black"}} className="mb-5"><MDBIcon icon="clone" className="mr-2"></MDBIcon> View project</MDBBtn>
         </MDBCol>
       </MDBCol>
     </MDBJumbotron>
