1d1f9f603f9334194e53a1d7ec216ad13b4a6622
Farid <farid.hammouten@gmail.com>
2020-11-14 03:51:26 +0900

    4 sections added

--- src/views/HomePage/Home.jsx
+++ src/views/HomePage/Home.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import Jumbotron from '../../components/Jumbotron';
+import Features from '../../components/Features';
+import FeaturesV2 from '../../components/FeaturesV2';
+import TabsJustified from '../../components/TabsJustified';
+
+import { MDBContainer } from 'mdbreact';
+
 
 const Home = () =>(
-    <Jumbotron />
+  <>
+    <section id="section1">
+      <Jumbotron />
+    </section>
+    <MDBContainer>
+      <section id="section2">
+        <Features />
+      </section>
+      <section id="section3">
+        <FeaturesV2 />
+      </section>
+      <section id="section4">
+        <TabsJustified />
+      </section>
+    </MDBContainer>
+
+  </>
 );
 
 export default Home;
