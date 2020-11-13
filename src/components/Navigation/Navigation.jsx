e0b873d6f1f2187fef6935e25501648b54a441ef
Farid <farid.hammouten@gmail.com>
2020-11-13 17:00:23 +0900

    updated Navigation with mdbreact component

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,21 +1,75 @@
 import React from 'react';
+import {
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarNav,
+  MDBNavbarToggler,
+  MDBCollapse,
+  MDBNavItem,
+  MDBNavLink,
+  MDBContainer,
+  MDBMask,
+  MDBView,
+  MDBIcon
+ } from 'mdbreact';
 import { Link } from 'react-router-dom';
 
-// components bootstrap
-import { Navbar, Nav } from 'react-bootstrap';
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: false,
+      isWideEnough: false,
+    };
+    this.onClick = this.onClick.bind(this);
+  }
 
-const Navigation = () =>(
-    <Navbar bg="primary" expand="lg" variant="dark">
-    <Navbar.Brand>JSSH</Navbar.Brand>
-    <Navbar.Toggle aria-controls="navbar-nav" />
-    <Navbar.Collapse id="navbar-nav">
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/">Home</Nav.Link>
-        <Nav.Link as={Link} to="/about">About</Nav.Link>
-        <Nav.Link as={Link} to="/faq">FAQ</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
-) 
+  onClick() {
+    this.setState({
+      collapse: !this.state.collapse,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <>
+            <MDBNavbar color="bg-primary" fixed="top" light expand="md" scrolling transparent>
+              <MDBNavbarBrand href="/">
+                <strong>JSSH</strong>
+              </MDBNavbarBrand>
+              {!this.state.isWideEnough && <MDBNavbarToggler onClick={this.onClick} />}
+              <MDBCollapse isOpen={this.state.collapse} navbar>
+                <MDBNavbarNav left>
+                  <MDBNavItem active>
+                    <MDBNavLink to="#section1">Section1</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#Section2">Section2</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink to="#Section3">Section3</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+                <MDBNavbarNav right>
+                  <MDBNavItem>
+                    <MDBNavLink as={Link} to="/">Page1</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink as={Link} to="/about">Page2</MDBNavLink>
+                  </MDBNavItem>
+                  <MDBNavItem>
+                    <MDBNavLink as={Link} to="/faq">Page3</MDBNavLink>
+                  </MDBNavItem>
+                </MDBNavbarNav>
+              </MDBCollapse>
+            </MDBNavbar>
+          </>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default Navigation;
